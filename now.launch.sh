a60ee23153f88af242c016b826e822203c3bfb8e
Alex Arendsen <alex.arendsen.7@gmail.com>
2018-08-27 12:30:08 -0500

    Added now.sh launch script

---
+++ now.launch.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Deployment script for Now.sh
+# Requires JWT_SECRET, MONGODB_USER, and MONGODB_SECRET to be defined in .env.production
+#
+# Example .env.production:
+#
+# export JWT_SECRET=SomethingToSignJWTsWith
+# export MONGODB_USER=nulist
+# export MONGODB_SECRET=PasswordForNulistMongoUser
+
+export ./.env.production
+
+# Have to do this since we're using the OSS plan and we don't want our
+# secrets on display for the world to see
+now --public -e JWT_SECRET=${JWT_SECRET} -e MONGODB_USER=${MONGODB_USER} -e MONGODB_SECRET=${MONGODB_SECRET}
+
