803deba5f0a36113596e6c9580c0480d4c7515f7
Alex Arendsen <alex.arendsen.7@gmail.com>
2018-09-07 13:36:35 -0500

    Helper that gets user from JWT now returns null on error

--- services/users.js
+++ services/users.js
@@ -3,12 +3,10 @@ const jwt = require('jsonwebtoken');
 module.exports = {
 
   getUserFromRestifyRequest: async function(request, db) {
-    const reject = () => { throw new Error(`No user found (token: ${request.headers.authorization})`); }
-
     const decoded = jwt.decode(request.headers.authorization);
-    if (!decoded || !decoded.username) reject();
+    if (!decoded || !decoded.username) return null;
     const user = await db.getUserByUsername(decoded.username);
-    return user || reject();
+    return user || null;
   }
 
 }
